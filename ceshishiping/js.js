d19f57e3735e085ee9e8de8193b16e0fd2c8d296
雪猫 <95599612+qinglaoleshi@users.noreply.github.com>
2024-02-05 20:13:19 +0800

    Add files via upload

---
+++ ceshishiping/js.js
@@ -0,0 +1,6 @@
+function init(){
+	window.a=["测试"];
+	window.late=document.getElementById("d");		//创建一个全局变量late用于保存上次播放的电影
+	window.late_1=document.getElementById("d");		//创建一个全局变量late_1用于保存上次悬浮的div
+    video.src="	";
+}
